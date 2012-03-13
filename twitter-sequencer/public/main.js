6f434a6e92f1b8d93f8d9daead7a8fcd3c47cab5
KirbySaysHi <kirbysayshi@gmail.com>
2012-03-13 01:49:34 -0400

    client-side time

--- twitter-sequencer/public/main.js
+++ twitter-sequencer/public/main.js
@@ -1,85 +1,224 @@
 (function($, exports, undefined){
 
-exports.twsq = {
+var twsq = exports.twsq = {
 
-	parseHash: function(hash){
+	parse: {
+		 app_id: 'bZHaUoeyWlnVCPAJv8OruDlWlpgSIE8oeIidKWgE'
+		,rest_key: 'xNXoVEJHFglGqVctUAZB7sha3osETNt7asivmkvi'
 
-		// try for #####,####,#####
-
-		// try for ######-#######,####
-
-		var  done = $.Deferred()
-			,sections = hash.slice(1).split(',')
-			,all = [];
-
-		sections.forEach(function(s){
-			
-			// greater than 0 because we want to guarantee at least #-#
-			//if(s.indexOf('-') > 0){
-			//	
-			//}
-
-			all.push(s);
-		});
-
-		done.resolve(all);
-
-		return done;
-	}
-
-	,grabTweet: function(id){
-
-		return $.getJSON('https://api.twitter.com/1/statuses/oembed.json?callback=?&omit_script=true&id=' + id);
-	}
-
-	,renderTweets: function(htmls){
-		var front = '<div class="tweet-wrap">'
-			,end = '</div>';
-
-		$('div[role=main]').append( front + htmls.join(end + front) + end );
-		$('<script/>', {
-			src: 'http://platform.twitter.com/widgets.js'
-		}).appendTo('body');
-	}
-
-	,startDisplay: function(){
-
-		var ids = twsq.parseHash(window.location.hash);
-
-		ids.done(function(idArr){
-
-			var idDfds = [];
-
-			if(idArr.length < 2) {
-				return;
-			};
-
-			idArr.forEach(function(id){
-				idDfds.push( twsq.grabTweet(id) );
-			})
-
-			$.when.apply($, idDfds).done(function(){
-				var htmls = [].slice.call(arguments).map(function(tweetResp){
-
-					if(tweetResp[1] === 'success'){
-						return tweetResp[0].html;
-					}
-
+		,getSequence: function(parseId){
+			var cached;
+			if( window.localStorage && (cached = window.localStorage[parseId]) ){
+				return new $.Deferred(function(dfd){
+					dfd.resolve( JSON.parse(cached) );
 				});
+			}
 
-				twsq.renderTweets(htmls);
-			})
+			return $.parse.get('tsequences/' + parseId 	).done(function(sequence){
+				if(window.localStorage){
+					window.localStorage[parseId] = JSON.stringify(sequence);
+				}
+			});
+		}
 
+		,saveSequence: function(tweets){
+			var m = new TweetSequenceModel({ tweets: tweets })
+			return $.parse.post('tsequences', { body: m.toJSON() });
+		}
+	}
+
+	,tweets: {
+
+		 apiBase: 'https://api.twitter.com/1'
+
+		,grabOne: function(id){
+			return $.getJSON(twsq.tweets.apiBase + '/statuses/show/' + id + '.json?callback=?');
+		}
+
+		,grabMany: function(arr){
+
+			var dfds = [];
+
+			arr.forEach(function(t){
+
+				if(t.indexOf('-') > -1){
+
+				} else {
+					dfds.push(twsq.tweets.grabOne(t));
+				}
+			});
+
+			return $.when.apply($, dfds);
+		}
+
+		,cleanTweet: function(tweet){
+			var  t = tweet.trim()
+				,last = t[ t.length - 1 ]
+
+			if(['.','!', '?', '"'].indexOf(last) === -1){
+				t += '.'
+			}
+
+			return t;
+		}
+	}
+
+	,parseIds: function(ids){
+
+		// simple, for now
+		return ids.split(',');
+	}
+
+	,error: function(){
+		console.log('error', arguments);
+	}
+}
+
+var TweetSequenceModel = Backbone.Model.extend({
+	defaults: {
+		tweets: []
+	}
+});
+
+var CreateView = Backbone.View.extend({
+
+	events: {
+		 'click a.add-tweet': 'addItem'
+		,'click a.remove-tweet': 'removeItem'
+		,'click a.create-sequence': 'makeNew'
+	}
+
+	,initialize: function(){
+		for(var i = 0; i < 5; i++){
+			this.addItem();
+		}
+	}
+
+	,addItem: function(e){
+
+		if(e){ 
+			e.stopPropagation(); 
+			e.preventDefault(); 
+		}
+
+		this.$el.find('.items')
+			.append( vash.compile( $('#new-item').html() )() );
+	}
+
+	,removeItem: function(e){
+
+		if(e){ 
+			e.stopPropagation(); 
+			e.preventDefault(); 
+		}
+
+		$(e.currentTarget).closest('.new-item').remove();
+	}
+
+	,makeNew: function(e){
+
+		if(e){ 
+			e.stopPropagation(); 
+			e.preventDefault(); 
+		}
+
+		var ids = _.reduce(this.$el.find('.new-item input'), function(memo, next){
+			var parts = $(next).val().split('/');
+
+			if(parts.length >= 1 && parts[0] !== ''){
+				memo.push( parts.pop() );
+			}
+
+			return memo;
+		}, []);
+
+		if(ids.length <= 1) return;
+
+		twsq.app.navigate( 'c/' + ids.join(','), { trigger: true } );
+	}
+
+});
+
+var Router = Backbone.Router.extend({
+
+	routes: {
+		 'i/:parseId': 'inline'
+		,'b/:parseId': 'block'
+		,'c/:ids': 'create'
+		,'404': 'notFound'
+	}
+
+	,initialize: function(){
+		$.parse.init({
+			 app_id : twsq.parse.app_id
+			,rest_key : twsq.parse.rest_key
 		});
+	}
+
+	,inline: function(parseId){
+		var tpl = vash.compile($('#tweet-inline').html());
+		this.render(parseId, tpl);
+	}
+
+	,block: function(parseId){
+		var tpl = vash.compile($('#tweet-block').html());
+		this.render(parseId, tpl);
+	}
+
+	,render: function(parseId, tpl){
+		twsq.parse.getSequence(parseId).then(function(sequence){
+			if(sequence.objectId !== null){
+
+				$('.main-content').html( tpl( sequence ) );
+			}
+		})
+	}
+
+	,create: function(ids){
+
+		// show loading template
+		$('.main-content').html( vash.compile( $('#creating-new').html() ) );
+		window.scrollTo(0,1);
+
+		// parse ids
+		var ids = twsq.parseIds(ids);
+
+		// query twitter for all tweets
+		var tweetDfd = twsq.tweets.grabMany(ids);
+
+		// when done, construct parse request
+		tweetDfd.then(function(){
+
+			var tweets = [].slice.apply(arguments).map(function(resp){
+				var t = resp[0];
+				t.text = twsq.tweets.cleanTweet( t.text );
+
+				return t;
+			});
+
+			twsq.parse.saveSequence( tweets ).then(function(json){
+
+				// when done, by default, redirect to inline view
+				twsq.app.navigate( 'b/' + json.objectId, { trigger: true } )
+
+			}, twsq.error);
+
+		}, twsq.error);
+	}
+
+	,notFound: function(){
 
 	}
-};
+
+});
+
+twsq.app = new Router;
 
 $(function(){
-	if(window.location.hash !== ''){
-		twsq.startDisplay();
-	}
-})
+	// wait for DOMReady to allow templates to be ready
+	Backbone.history.start();
 
+	new CreateView({ el: '.create-sequence' });
+})
 
 })(jQuery, window);
